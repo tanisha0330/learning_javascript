99d92e72c44d8db1fc7c505751407fe216c3d165
Tuti_tata <tanishajaiswal20042004@gmail.com>
2025-06-02 03:35:13 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
-console.log("tanisha")
+
+const accountId=144553
+let accountEmail = "tanisha@google.com"
+var accountPassword= "12345"
+accountCity="random"
+accountEmail = "hc@hc.com"
+accountPassword="kk"
+accountCity= "lalitpur"
+let accountState ; 
+
+//accountId = 2  not allowed 
+
+console.log(accountId);
+
+console.table([accountEmail, accountId , accountPassword,accountCity, accountState])
+
+
+// variables declare karne k 2 ways : var ans let . 
+// {} = scope 
+/* prefer not to use var becoz of 
+fuctional issue  in block scope 
+and fuctional scope */ 
